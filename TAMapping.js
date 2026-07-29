00e406efdf67c0eceb67d430e4834e848b548231
ravikumarmalhotra <ravikumarmalhotra343@gmail.com>
2026-07-29 14:02:26 +0530

    Update TAMapping.js

--- TAMapping.js
+++ TAMapping.js
@@ -1,8 +1,29 @@
 // ===== TAMapping.js =====
 // Keys use USER_NAME format (uppercase, spaces -> underscore)
 const TAMapping = {
-  "RAVI_KUMAR_MALHOTRA": {Designation: "TECH-I", PF_No: "52229800571",Mob_No: "9821567620", Bill_Unit: "2201-225", Basic_Pay: "₹30,100", Scale: "L-5", Date_Of_Appointment: "30-07-2020" ,Rates: "625"}
-  // add more employees...
+"NARENDRA_EKKA" = {Designation: "SSE", PF_No: "22199929697",Mob_No: "9007041492", Bill_Unit: "2201-226", Basic_Pay: "₹81,200", Scale: "L-8", Date_Of_Appointment: "02-12-1999" ,Rates: "1000"},
+"KISHOR_KUMAR" = {Designation: "JE", PF_No: "52229800864",Mob_No: "9821563845", Bill_Unit: "2201-225", Basic_Pay: "₹39,900", Scale: "L-6", Date_Of_Appointment: "03-11-2022" ,Rates: "1000"}'
+"MD._AMIR_ANSARI" = {Designation: "JE", PF_No: "52229800865",Mob_No: "9821567336", Bill_Unit: "2201-225", Basic_Pay: "₹39,900", Scale: "L-6", Date_Of_Appointment: "03-11-2022" ,Rates: "1000"},
+"ARPAN_MONDAL" = {Designation: "JE", PF_No: "52229800862",Mob_No: "9821563883", Bill_Unit: "2201-225", Basic_Pay: "₹39,900", Scale: "L-6", Date_Of_Appointment: "03-11-2022" ,Rates: "1000"},
+"RAJA_MANDAL" = {Designation: "SSE", PF_No: "22177701999",Mob_No: "9007041464", Bill_Unit: "2201-229", Basic_Pay: "₹47600", Scale: "L-7", Date_Of_Appointment: "26-07-2006" ,Rates: "1000"},
+"RAVI_KUMAR_MALHOTRA" = {Designation: "TECH-I", PF_No: "52229800571",Mob_No: "9821567620", Bill_Unit: "2201-225", Basic_Pay: "₹30,100", Scale: "L-5", Date_Of_Appointment: "30-07-2020" ,Rates: "625"},
+"NITISH_KUMAR" = {Designation: "TECH-I", PF_No: "52229800634",Mob_No: "9821567135", Bill_Unit: "2201-225", Basic_Pay: "₹30,100", Scale: "L-5", Date_Of_Appointment: "28-09-2020" ,Rates: "625"},
+"SAMBHU_DAS" = {Designation: "TECH-III", PF_No: "52229800892",Mob_No: "9821567147", Bill_Unit: "2201-225", Basic_Pay: "₹21,700", Scale: "L-2", Date_Of_Appointment: "29-11-2022" ,Rates: "625"},
+"SURAJ_THAKUR" = {Designation: "TECH-III", PF_No: "52229800891",Mob_No: "9821564801", Bill_Unit: "2201-225", Basic_Pay: "₹21,700", Scale: "L-2", Date_Of_Appointment: "30-11-2022" ,Rates: "625"},
+"NITISH_KUMAR_CHOUDHARY" = {Designation: "TECH-III", PF_No: "52229800909",Mob_No: "9821564468", Bill_Unit: "2201-225", Basic_Pay: "₹21,700", Scale: "L-2", Date_Of_Appointment: "26-12-2022" ,Rates: "625"},
+"SUMAN_KUMAR" = {Designation: "TECH-III", PF_No: "52229801065",Mob_No: "9821564407", Bill_Unit: "2201-226", Basic_Pay: "₹21,700", Scale: "L-2", Date_Of_Appointment: "09-01-2023" ,Rates: "625"},
+"SHIVAM_KUMAR" = {Designation: "HELPER", PF_No: "52229801105",Mob_No: "8804592136", Bill_Unit: "2201-226", Basic_Pay: "₹19,100", Scale: "L-1", Date_Of_Appointment: "05-09-2023" ,Rates: "625"},
+"SUMAN_THAKUR" = {Designation: "HELPER", PF_No: "52229801145",Mob_No: "9932743471", Bill_Unit: "2201-226", Basic_Pay: "₹19,100", Scale: "L-1", Date_Of_Appointment: "05-09-2023" ,Rates: "625"},
+"SUBRATA_DAS" = {Designation: "HELPER", PF_No: "52229801116",Mob_No: "9614073925", Bill_Unit: "2201-226", Basic_Pay: "₹19,100", Scale: "L-1", Date_Of_Appointment: "05-09-2023" ,Rates: "625"},
+"BIKESH_SINGH" = {Designation: "HELPER", PF_No: "52229801125",Mob_No: "7074045320", Bill_Unit: "2201-226", Basic_Pay: "₹19,100", Scale: "L-1", Date_Of_Appointment: "28-08-2023" ,Rates: "625"},
+"RAUSHAN_KUMAR_JOSHI" = {Designation: "HELPER", PF_No: "52229801150",Mob_No: "7859927029", Bill_Unit: "2201-226", Basic_Pay: "₹19,100", Scale: "L-1", Date_Of_Appointment: "21-09-2023" ,Rates: "625"},
+"ANIL_SINGH" = {Designation: "HELPER", PF_No: "52229801151",Mob_No: "9616037393", Bill_Unit: "2201-226", Basic_Pay: "₹19,100", Scale: "L-1", Date_Of_Appointment: "21-09-2023" ,Rates: "625"},
+"AMAN_BHADURIA" = {Designation: "HELPER", PF_No: "52229801185",Mob_No: "6205761308", Bill_Unit: "2201-226", Basic_Pay: "₹19,100", Scale: "L-1", Date_Of_Appointment: "09-11-2023" ,Rates: "625"},
+"SURAJ_KUMAR" = {Designation: "HELPER", PF_No: "52229801192",Mob_No: "7004822621", Bill_Unit: "2201-226", Basic_Pay: "₹19,100", Scale: "L-1", Date_Of_Appointment: "29-11-2023" ,Rates: "625"},
+"SUMAN_KRISHNA_ROY" = {Designation: "HELPER", PF_No: "18729803376",Mob_No: "7980225818", Bill_Unit: "2201-226", Basic_Pay: "₹19,700", Scale: "L-1", Date_Of_Appointment: "23-05-2023" ,Rates: "625"},
+"DHARMENDRA_KUMAR" = {Designation: "HELPER", PF_No: "52229801227",Mob_No: "8407817047", Bill_Unit: "2201-226", Basic_Pay: "₹19,100", Scale: "L-1", Date_Of_Appointment: "06-12-2023" ,Rates: "625"},
+"RAVI_RANJAN_KUMAR" = {Designation: "HELPER", PF_No: "52229801225",Mob_No: "8862839648", Bill_Unit: "2201-226", Basic_Pay: "₹19,100", Scale: "L-1", Date_Of_Appointment: "12-12-2023" ,Rates: "625"},
+"MANISH_KUMAR" = {Designation: "HELPER", PF_No: "52229801224",Mob_No: "9523021389", Bill_Unit: "2201-226", Basic_Pay: "₹19,100", Scale: "L-1", Date_Of_Appointment: "12-12-2023" ,Rates: "625"}
 };
 
 // "To" dropdown options
