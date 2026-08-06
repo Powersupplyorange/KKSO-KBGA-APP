e8be24f1b88942387ba9148978f0e23d7740a7e7
ravikumarmalhotra <ravikumarmalhotra343@gmail.com>
2026-08-06 13:01:32 +0530

    Reset SerialNo in payload for non-editing state
    Reset SerialNo to an empty string when not editing.

--- Entry/TA Form/Test/TAscript3.js
+++ Entry/TA Form/Test/TAscript3.js
@@ -227,7 +227,7 @@ async function handleSubmit(e) {
   const payload = {
     target: targetMonth,
     data: {
-      SerialNo: isEditing ? editingSerialNo : '',
+      SerialNo: '',
       NameOfEmployee: fldName.value,
       Designation: fldDesignation.value,
       Date: dateFormatted,
