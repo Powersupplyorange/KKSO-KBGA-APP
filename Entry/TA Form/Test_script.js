2939324d61e90e65248231df92ffc34dfcdd0373
ravikumarmalhotra <ravikumarmalhotra343@gmail.com>
2026-08-08 19:21:40 +0530

    Update Test_script.js

--- Entry/TA Form/Test_script.js
+++ Entry/TA Form/Test_script.js
@@ -111,7 +111,12 @@ function initEntryForm() {
   fldDesignation.value = employeeData.Designation || '';
 
   const today = new Date();
-  const minD = new Date(today); minD.setDate(today.getDate() - 1);
+
+const minD = new Date(today);
+// Only subtract 1 day if today is NOT the 1st day of the month
+if (today.getDate() > 1) {
+  minD.setDate(today.getDate() - 1);
+}
   const maxD = new Date(today); maxD.setDate(today.getDate() + 0);
   fldDate.min = formatDateYMD(minD);
   fldDate.max = formatDateYMD(maxD);
